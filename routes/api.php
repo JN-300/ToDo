59423766277712924ab417aa6f38adc88a33df9a
Jeffrey Nellissen <jene@dcc.ruhr>
2024-07-06 12:15:16 +0200

    enable sqlite memory db for tests add lang files finish simple sanctum base token authorization

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::name('token.' )
+    ->prefix('token')
+    ->group(function () {
+        Route::post('/', [\App\Http\Controllers\Api\TokenController::class, 'createToken'])
+            ->name('create');
+
+        Route::delete('/', [\App\Http\Controllers\Api\TokenController::class, 'deleteToken'])
+            ->name('delete')
+            ->middleware('auth:sanctum')
+        ;
+    });
+
+
+
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json($request->user());
 })->middleware('auth:sanctum');
